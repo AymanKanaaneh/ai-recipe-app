dcb173efa7466a2d3cfe410b04bb0cf95e6b44b7
Ayman Hussein <ayman.kanaaneh@gmail.com>
2025-10-18 20:35:35 +0300

    feat: Implement frontend routing and auth guard

---
+++ frontend/recipe-ui/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { UploadComponent } from './upload/upload.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'upload', component: UploadComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
